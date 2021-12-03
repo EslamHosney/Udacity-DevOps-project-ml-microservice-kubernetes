42957648be25b4e89540d9f355c6d028e80fc998
Eslam Hosney <eslamsamy.m.eg@gmail.com>
2021-12-03 14:42:15 +0000

    Kubectl running El7

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=eslamhosney/udacity_ml_proj_img
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker pull eslamhosney/udacity_ml_proj_img:latest
+kubectl run udacity-ml-proj-deploy\
+	    --image=eslamhosney/udacity_ml_proj_img\
+		--port=80 --labels app=udacity-ml-proj-deploy
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
 
+kubectl port-forward udacity-ml-proj-deploy 8000:80
