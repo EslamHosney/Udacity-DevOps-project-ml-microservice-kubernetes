a48d2e39832003d45f6c82042aa76a7e37bb14ba
Eslam Hosney <eslamsamy.m.eg@gmail.com>
2021-12-03 14:25:47 +0000

    Update upload_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=udacity_ml_proj_img .
+docker build --tag=eslamhosney/udacity_ml_proj_img .
 
 # Step 2: 
 # List docker images
@@ -14,4 +14,4 @@ docker image ls
 # Step 3: 
 # Run flask app
 
-docker run -p 127.0.0.1:8000:80/tcp udacity_ml_proj_img
+docker run -p 127.0.0.1:8000:80/tcp eslamhosney/udacity_ml_proj_img
