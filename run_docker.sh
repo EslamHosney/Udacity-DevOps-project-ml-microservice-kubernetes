0cf9c6655e3452818b82156e49c83e80ad267f93
Eslam Hosney <eslamsamy.m.eg@gmail.com>
2021-12-03 12:18:45 +0000

    Fix CircleCI file

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,14 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=udacity_ml_proj_img .
 
 # Step 2: 
 # List docker images
 
+docker image ls
+
 # Step 3: 
 # Run flask app
+
+docker run -p 8000:80 -it udacity_ml_proj_img bash
